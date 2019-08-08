0622cd9f83c2ce59b9a9fa7b27855d92443c5278
Corey Ladovsky <coreyskymusic@gmail.com>
2019-08-08 15:03:00 -0400

    almost

--- index.js
+++ index.js
@@ -16,17 +16,43 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.json())
     .then(res => {
       let body = document.querySelector("body");
-      let ul = document.createElement("ul");
-      body.appendChild(ul);
+      let select = document.createElement("select");
+      body.appendChild(select);
 
       res.forEach(movieObj => {
-        let li = document.createElement("li");
+        let option = document.createElement("option");
         let title = movieObj.title;
-        li.innerText = title;
-        ul.appendChild(li)
+        option.innerText = title;
+        select.appendChild(option)
       })
 
+      return res;
     })
+    .then(res => {
+      let select = document.querySelector("select");
+      select.addEventListener("change", (e) => {
+        let movie = res.find(el => el.title === e.target.value)
+        let div = document.createElement("div")
+        div.classList.add("displayDiv")
+        let title = document.createElement("h1")
+        let description = document.createElement("h3");
+        title.innerText = movie.title;
+        description.innerText = movie.description;
+        div.appendChild(title)
+        div.appendChild(description)
+        let oldDiv = document.querySelector("displayDiv");
+        let body = document.querySelector("body");
+        if(oldDiv) {
+          body.removeChild(oldDiv)
+        }
+        body.appendChild(div)
+
+
+      })
+
+
+    })
+
 
 
 });
